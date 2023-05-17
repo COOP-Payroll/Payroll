c8b71aa68449ad879c7ec809c94b8a729da344ea
Lelisa Yohannes Tarressa <lelisay@coopbank.local>
2023-05-17 02:30:50 +0000

    23

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const run = require("./utils/checkSubscriptionPlan");
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
-const app = express();
+
 const sequelize = require("./database/db.js");
 
 const userRouter = require("./routes/user.js");
@@ -11,15 +12,33 @@ const taxslabRouter = require("./routes/taxslab.js");
 const pensionRouter = require("./routes/pension.js");
 const deptRouter = require("./routes/department.js");
 const subscriptionRouter = require("./routes/subscription.js");
-const allowance = require('./routes/allowance')
-const allowanceDefinition = require('./routes/allowanceDefinition')
-const deduction = require('./routes/deduction')
-const grade = require('./routes/grade')
-const deductionDefinition = require('./routes/deductionDefinition')
+const authRouter = require("./routes/auth.js");
+const companyIdRouter = require("./routes/companyId");
+const allowance = require("./routes/allowance");
+const allowanceDefinition = require("./routes/allowanceDefinition");
+const deduction = require("./routes/deduction");
+const grade = require("./routes/grade");
+const deductionDefinition = require("./routes/deductionDefinition");
+const payrollRouter = require("./routes/payroll");
+const employeeRouter = require("./routes/employee.js");
 const approvalMethod = require("./routes/approvalMethod")
-const payroll = require("./routes/payrollDefinition")
-const cron = require("node-cron");
-const bodyParser = require("body-parser");
+const payrollDefinition = require("./routes/payrollDefinition")
+
+const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://localhost:3002",
+      "http://localhost:*",
+      "http://10.2.125.124:4000",
+      "*",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -38,7 +57,8 @@ app.use("/pension", pensionRouter);
 app.use("/department", deptRouter);
 app.use("/subscription", subscriptionRouter);
 app.use("/approvalmethod", approvalMethod);
-app.use("/Payroll", payroll);
+app.use("/payrollDefinition", payrollDefinition);
+
 
 sequelize.sync().then(() => console.log("db is ready"));
 
