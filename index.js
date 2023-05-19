0db38dcc3363f7b25076faa20e5a60808e625c62
unknown <eske17.ea@gmail.com>
2023-05-19 17:14:53 +0300

    update deduction table

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const authRouter = require("./routes/auth.js");
 const companyIdRouter = require("./routes/companyId");
 const allowance = require("./routes/allowance");
 const allowanceDefinition = require("./routes/allowanceDefinition");
-const loanDefinition=require('./routes/loanDefinition');
+const loanDefinition = require("./routes/loanDefinition");
 const deduction = require("./routes/deduction");
 const grade = require("./routes/grade");
 const deductionDefinition = require("./routes/deductionDefinition");
@@ -24,8 +24,8 @@ const payrollRouter = require("./routes/payroll");
 const employeeRouter = require("./routes/employee.js");
 const payrollDefinition = require("./routes/payrollDefinition");
 const approvalMethod = require("./routes/approvalMethod");
-const customRoleRouter=require('./routes/customRole.js');
-const loanRoute=require('./routes/loan.js')
+const customRoleRouter = require("./routes/customRole.js");
+const loanRoute = require("./routes/loan.js");
 
 const app = express();
 
@@ -60,8 +60,8 @@ app.use("/companyIdFormat", companyIdRouter);
 app.use("/allowancedefinition", allowanceDefinition);
 app.use("/allowance", allowance);
 app.use("/deductiondefinition", deductionDefinition);
-app.use('/loanDefinition',loanDefinition)
-app.use("/loan",loanRoute)
+app.use("/loanDefinition", loanDefinition);
+app.use("/loan", loanRoute);
 app.use("/deduction", deduction);
 app.use("/grade", grade);
 app.use("/payroll", payrollRouter);
