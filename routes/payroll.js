77c4c8b8608394ada48e9421549a6bf2054452bc
gemechu14 <gemechubulti11@gmail.com>
2023-05-23 08:46:00 +0300

    provident Fund

--- routes/payroll.js
+++ routes/payroll.js
@@ -1,21 +1,13 @@
 const express = require("express");
 const payroll = require("../controllers/payrollController");
 const middleware = require("../middleware/auth");
-<<<<<<< HEAD
-=======
-
->>>>>>> 3405f716c94d163ff52dee6724b2da0b7b23c3aa
 const router = express.Router();
 
 router.get("/", payroll.getAllPayrollByCompanyId);
 router.post(
   "/",
   middleware.protectAll,
-<<<<<<< HEAD
-  middleware.restrictTo("companyAdmin"),
-=======
   middleware.restrictToAdmin("companyAdmin"),
->>>>>>> 3405f716c94d163ff52dee6724b2da0b7b23c3aa
   payroll.createPayroll
 );
 
