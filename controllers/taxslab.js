5dc1cc1351751ee0c96d24d31d91126ae1f4ca1a
Lelisa Yohannes Tarressa <lelisay@coopbank.local>
2023-05-31 10:34:36 +0000

    set approval level

--- controllers/taxslab.js
+++ controllers/taxslab.js
@@ -4,7 +4,9 @@ const User = require("../models/user.js");
 
 // Define controller methods for handling User requests
 exports.getAllTaxslabs = async (req, res) => {
+
   try {
+    
     if (req.user.role === "superAdmin") {
       const taxslab = await Taxslab.findAll({
         where: { userId: req.user.id, isActive: true },
