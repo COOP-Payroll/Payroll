27866eac97b247b81e089fbb040a185565c071c9
gemechu14 <gemechubulti11@gmail.com>
2023-06-06 14:53:28 +0300

    address

--- controllers/employeeControllers.js
+++ controllers/employeeControllers.js
@@ -928,15 +928,19 @@ exports.login = async (req, res, next) => {
         {
           model: Company,
           where: { companyCode: companyCode },
+         
         },
         {
           model: CustomRole,
           include: [Permission],
         },
+        {
+          model:Address
+        }
       ],
     });
     // console.log("user", user);
-    console.log("password", await bcrypt.compare(password, user.password));
+   // console.log("password", await bcrypt.compare(password, user.password));
     if (!user || !(await bcrypt.compare(password, user.password))) {
       return res.status(401).json({ error: "Incorrect email, password" });
       //next(createError.createError(401,'Incorrect email, password or company Code'))
