48c33ad92f9fa72cf3c5c6d733850062d3a2a4fe
unknown <gemechubulti11@gmail.com>
2024-12-06 16:04:07 +0300

    updated

--- database/db.js
+++ database/db.js
@@ -101,60 +101,22 @@ require("dotenv").config();
 
 //Local server
 
-require("dotenv").config();
-const { Sequelize } = require("sequelize");
-// const CustomError = require("../utils/ErrorHandler");
-const sequelize = new Sequelize({
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || "5432",
-  database: process.env.DB_NAME || "",
-  username: process.env.DB_USER || "",
-  password: process.env.DB_PASSWORD || "",
-  dialect: "postgres",
-  pool: {
-    max: 500, // Increase the maximum number of connections
-    min: 0, // Minimum number of connections
-    acquire: 300000, // Maximum time (in ms) to try getting a connection
-    idle: 100000, // Time (in ms) before releasing an idle connection
-  },
-});
-// Test the database connection
-async function testConnection() {
-  try {
-    await sequelize.authenticate();
-    console.log("Database connection has been established successfully.");
-  } catch (error) {
- 
-    console.error("Error connecting");
-  }
-}
-
-// sequelize
-//   .sync({ alter: true }) // Use force: true carefully, as it drops existing tables
-//   .then(() => {
-//     console.log("Database synchronized successfully.");
-//   })
-//   .catch((error) => {
-//     console.error("Erro r synchronizing database:", error);
-//   });
-
-testConnection();
-module.exports = sequelize;
-
-///RENDER
-
 // require("dotenv").config();
 // const { Sequelize } = require("sequelize");
 // // const CustomError = require("../utils/ErrorHandler");
-// const sequelize = new Sequelize('postgres://ct_user:y3sNLAB4dr7pcq8pCBqwMtoCzMsvrZq1@dpg-cp3gmi7sc6pc73foh22g-a.oregon-postgres.render.com/ct',{
-
+// const sequelize = new Sequelize({
+//   host: process.env.DB_HOST || "localhost",
+//   port: process.env.DB_PORT || "5432",
+//   database: process.env.DB_NAME || "",
+//   username: process.env.DB_USER || "",
+//   password: process.env.DB_PASSWORD || "",
 //   dialect: "postgres",
-//   dialectOptions: {
-//     ssl: {
-//       require: true, // This will help in ensuring SSL connection
-//       rejectUnauthorized: false // This might be necessary for some providers like Heroku
-//     }
-//   }
+//   pool: {
+//     max: 500, // Increase the maximum number of connections
+//     min: 0, // Minimum number of connections
+//     acquire: 300000, // Maximum time (in ms) to try getting a connection
+//     idle: 100000, // Time (in ms) before releasing an idle connection
+//   },
 // });
 // // Test the database connection
 // async function testConnection() {
@@ -162,11 +124,49 @@ module.exports = sequelize;
 //     await sequelize.authenticate();
 //     console.log("Database connection has been established successfully.");
 //   } catch (error) {
-//     console.log(error)
-//     console.log(process.env.DB_HOST)
+ 
 //     console.error("Error connecting");
 //   }
 // }
+
+// // sequelize
+// //   .sync({ alter: true }) // Use force: true carefully, as it drops existing tables
+// //   .then(() => {
+// //     console.log("Database synchronized successfully.");
+// //   })
+// //   .catch((error) => {
+// //     console.error("Erro r synchronizing database:", error);
+// //   });
+
+// testConnection();
+// module.exports = sequelize;
+
+///RENDER
+
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+// const CustomError = require("../utils/ErrorHandler");
+const sequelize = new Sequelize('postgresql://database_ilt0_user:xihY6VoS4jtxpY2o8w6UNMn4C5VqWBkG@dpg-ct9fajl6l47c73as14kg-a.oregon-postgres.render.com/database_ilt0',{
+
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help in ensuring SSL connection
+      rejectUnauthorized: false // This might be necessary for some providers like Heroku
+    }
+  }
+});
+// Test the database connection
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connection has been established successfully.");
+  } catch (error) {
+    console.log(error)
+    console.log(process.env.DB_HOST)
+    console.error("Error connecting");
+  }
+}
 // sequelize.sync({ alter: true }) // Use force: true carefully, as it drops existing tables
 //   .then(() => {
 //     console.log('Database synchronized successfully.');
@@ -175,5 +175,5 @@ module.exports = sequelize;
 //     console.error('Erro r synchronizing database:', error);
 //   });
 
-// testConnection();
-// module.exports = sequelize;
+testConnection();
+module.exports = sequelize;
