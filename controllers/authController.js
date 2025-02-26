73ba7e4880b2eb323908dd3d38dca0f05c8d6a70
unknown <gemechubulti11@gmail.com>
2025-02-26 11:29:33 +0300

    updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -123,7 +123,7 @@ const signTokenCompany = (company, res) => {
         isProjectBased,
         isSetted,
 
-        permissions: [
+        Permissions: [
           {
             module: "dashboard",
             isAccessible: true,
@@ -196,7 +196,7 @@ const signTokenCompany = (company, res) => {
         // Permissions,
         isProjectBased,
         isSetted,
-        permissions: [
+        Permissions: [
           {
             module: "dashboard",
             isAccessible: true,
