6cf67c0d688f88ee276dc14c837caddae7c7311d
Eskinder Abera <eske17.ea@gmail.com>
2025-05-17 12:34:52 +0300

    clean the project

--- controllers/logout.js
+++
@@ -1,6 +0,0 @@
-exports.logout = async (req, res, next) => {
-  try {
-  } catch (error) {
-    console.log("error", error);
-  }
-};
--- routes/stripePayment.js
+++
@@ -1,8 +0,0 @@
-// const express = require("express");
-// const router = express.Router();
-// const stripeController = require("../controllers/stripePayment.js");
-
-// router.post("/", stripeController.create_Payment);
-// router.get("/pay", stripeController.pay);
-// router.post("/charge", stripeController.charge);
-// module.exports = router;
