444092209601d0b2000e67c8ab4217d92e4042d8
Eskinder Abera <eske17.ea@gmail.com>
2025-05-20 12:49:34 +0300

    add position

--- src/routes/v1/position.route.ts
+++ src/routes/v1/position.route.ts
@@ -1,15 +1,15 @@
-import express from 'express';
-import positionController from '../../controllers/position.controller';
+import express from "express";
+import positionController from "../../controllers/position.controller";
 
 const router = express.Router();
 
 router
-  .route('/')
+  .route("/")
   .post(positionController.createPosition)
   .get(positionController.getAllPositions);
 
 router
-  .route('/:id')
+  .route("/:id")
   .get(positionController.getPositionById)
   .post(positionController.updatePosition)
   .delete(positionController.deletePosition);
