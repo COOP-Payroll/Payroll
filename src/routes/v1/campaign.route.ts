72ef5b0b0100b2374845633044e8ea5f74d01471
unknown <gemechubulti11@gmail.com>
2025-06-02 16:03:57 +0300

    updated

--- src/routes/v1/campaign.route.ts
+++ src/routes/v1/campaign.route.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import campaignController from "../../controllers/campaign.controller";
 import { upload } from "../../config/multer";
 import auth from "../../middlewares/auth";
@@ -41,20 +41,40 @@ router.post(
 
 // router.post("/document/:id", auth(), campaignController.deleteDocumentsByQuery);
 
+// router.post(
+//   "/process/:id",
+//   auth(),
+//   (next: NextFunction) => {
+//     console.log(">> Route hit, before multer");
+//     next();
+//   },
+//   upload.array("documents"),
+//   (next: NextFunction) => {
+//     console.log(">> After multer, files:");
+//     next();
+//   },
+//   multerErrorHandler,
+//   (next: NextFunction) => {
+//     console.log(">> After multerdddd, files:");
+//     next();
+//   },
+//   campaignController.processCampaign
+// );
+
 router.post(
   "/process/:id",
   auth(),
-  (next: NextFunction) => {
+  (req: Request, res: Response, next: NextFunction) => {
     console.log(">> Route hit, before multer");
     next();
   },
   upload.array("documents"),
-  (next: NextFunction) => {
-    console.log(">> After multer, files:");
+  (req: Request, res: Response, next: NextFunction) => {
+    console.log(">> After multer, files:", req.files);
     next();
   },
   multerErrorHandler,
-  (next: NextFunction) => {
+  (req: Request, res: Response, next: NextFunction) => {
     console.log(">> After multerdddd, files:");
     next();
   },
