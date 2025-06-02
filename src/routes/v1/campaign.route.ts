7a0407cdb4e52d0e372cfb1f74a10bee9b71f089
unknown <gemechubulti11@gmail.com>
2025-06-02 18:25:57 +0300

    updated

--- src/routes/v1/campaign.route.ts
+++ src/routes/v1/campaign.route.ts
@@ -66,7 +66,10 @@ router.post(
 router.post(
   "/process/:id",
   auth(),
-
+  (req: Request, res: Response, next: NextFunction) => {
+    console.log(">> Before multer, files:", req.files);
+    next();
+  },
   upload.array("documents"),
   (req: Request, res: Response, next: NextFunction) => {
     console.log(">> After multer, files:", req.files);
