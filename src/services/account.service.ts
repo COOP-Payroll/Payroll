e4f217234364e1fd7e395b8c4e1a8ecc49c44019
unknown <gemechubulti11@gmail.com>
2025-06-18 16:07:05 +0300

    account number verification

--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -220,6 +220,7 @@ export const verifyAccountByNumber = async (
         // Service_name: "accountEnquiryMC",
         // Message_Id: Date.now().toString(),
 
+
         serviceCode: "990000",
         channel: "USSD",
         Service_name: "accountEnquiryMC",
@@ -231,10 +232,10 @@ export const verifyAccountByNumber = async (
 
   let response;
   try {
-    console.log("kdkdkkdk", payload);
+ 
     response = await axios.post(url, payload);
   } catch (err) {
-    console.log("gemechu ", err);
+    
     throw new ApiError(
       httpStatus.SERVICE_UNAVAILABLE,
       "Failed to connect to account verification service"
